e9dad9c606fdd4a47830e5ea1e1086895cc0daaf
Paweł Szumański <pawel.szumanski@outlook.com>
2025-02-03 10:28:14 +0100

    Update ImageGallery.tsx

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -2,31 +2,15 @@ import { useEffect, useState } from "react";
 import { Picture } from "../interfaces/Picture";
 import css from './ImageGallery.module.css';
 
-export const ImageGallery = ({ images, perPage, handleImageClick, setIsGalleryLoading }: 
-    { images: Picture[], perPage: number, handleImageClick: (image: Picture) => void, setIsGalleryLoading: React.Dispatch<React.SetStateAction<boolean>> }) => {
-    
-    const [loadedImages, setLoadedImages] = useState(0);
-
-    useEffect(() => {
-        if (images.length <= perPage) {
-            setLoadedImages(0);
-        }
-        setIsGalleryLoading(true);
-    } , [images, perPage]);
-    
-    const handleImageLoad = () => {
-        setLoadedImages(prevLoadedImages => prevLoadedImages + 1);
-        if (loadedImages === images.length - 1) {
-            setIsGalleryLoading(false);
-        }
-    };
+export const ImageGallery = ({ images, perPage, handleImageClick, onImageLoad }: 
+    { images: Picture[], perPage: number, handleImageClick: (image: Picture) => void, onImageLoad: () => void }) => {
     
     return (
         <>
             <ul className={css.ImageGallery}>
                 {images.map(image => (
                     <li key={image.webformatURL} className={css.ImageGalleryItem} onClick={() => handleImageClick(image)}>
-                        <img src={image.webformatURL} alt={image.tags} onLoad={handleImageLoad} className={css.ImageGalleryItemImage} />
+                        <img src={image.webformatURL} alt={image.tags} onLoad={onImageLoad} className={css.ImageGalleryItemImage} />
                     </li>
                 ))}
             </ul>
